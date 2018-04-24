1156868ce7dffd2405457ba8793867431d859914
Sofikul <sofikul.mallick786@gmail.com>
2018-04-24 22:43:56 +0530

    Modfied image position in excel

--- index.js
+++ index.js
@@ -1,62 +1,88 @@
 const fs = require('fs');
 const Path = require('path');
-
 const Excel = require('exceljs');
 
 let workbook = null;
 let worksheet = null;
+
+
+let startRow = 0;
+let endRow = 0;
+
+const startCol = 'B';
+const endCol = 'Q';
+const imageHeight = 22;
+const gapBtwImg = 5;
+
+
+/**
+Create the excel file 
+**/
+fs.readdir(Path.join(__dirname, 'test-cases'), (err, items) => {
+	console.log(`No of items : ${items.length}`);
+
+	createExcel(items);
+
+});
+
 /**
 Creating excel file
 
 **/
-let createExcel = () => {
+let createExcel = (items) => {
 
+	_createWorkBook();
+	_addImages(items);
+	_saveExcel();
+	
+}
+
+let _createWorkBook = () => {
 	try {
         workbook = new Excel.Workbook();
         worksheet = workbook.addWorksheet('Test Execution');
 
-        worksheet.columns = [
+        /*worksheet.columns = [
             { header: 'Id', key: 'id', width: 10 },
             { header: 'Name', key: 'name', width: 32 },
             { header: 'D.O.B.', key: 'DOB', width: 10 }
         ];
         worksheet.addRow({id: 1, name: 'SofikulM', DOB: new Date(2018,4,22)});
-        worksheet.addRow({id: 2, name: 'Jane Doe', DOB: new Date(2018,4,22)});    
+        worksheet.addRow({id: 2, name: 'Jane Doe', DOB: new Date(2018,4,22)});  */  
 
     } catch(err) {
-        console.log('OOOOOOO this is the error: ' + err);
+        console.log(`Fail to create the Excel: ${err}`);
     }
 }
 
-let addImage = (fileName, ext) => {
+let _addImages = (items) => {
+
+	items.forEach( (i) => {
+		console.log(Path.join(__dirname, 'test-cases', i));
+		_addImage(Path.join(__dirname, 'test-cases', i), 'png');
+	})
+}
+
+let _addImage = (fileName, ext) => {
 	var img = workbook.addImage({
   		filename: fileName,
   		extension: ext,
 	});
 
 	console.log(img);
+	
+	
+	startRow = endRow + gapBtwImg;
+	endRow = startRow + imageHeight;
 
-	// insert an image over part of B2:D6
-	worksheet.addImage(img, 'B4:O24');
+	let imgPosition = `${startCol + startRow}:${endCol + endRow}`;
+
+	worksheet.addImage(img, imgPosition);
 }
 
-let saveExcel = () => {
+let _saveExcel = () => {
 	
 	workbook.xlsx.writeFile("./test-cases/Test Evidence.xlsx").then(function() {
-    		console.log("Excel(xlsx) file is written.");
+    	console.log("Excel(xlsx) file is written.");
 	});
 }
-
-
-fs.readdir(Path.join(__dirname, 'test-cases'), (err, items) => {
-	console.log(`No of items : ${items.length}`);
-
-	createExcel();
-
-	items.forEach( (i) => {
-		console.log(Path.join(__dirname, 'test-cases', i));
-		addImage(Path.join(__dirname, 'test-cases', i), 'png');
-	})
-
-	saveExcel();
-});
